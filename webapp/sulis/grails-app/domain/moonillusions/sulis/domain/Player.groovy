ca6cc9edbf08f3d37af9cb0b929b9fe279b920cc
ra1p3 <raipe.spamfilter@gmail.com>
2013-11-25 21:01:01 +0200

    NEW: Player domain object

---
+++ webapp/sulis/grails-app/domain/moonillusions/sulis/domain/Player.groovy
@@ -0,0 +1,10 @@
+package moonillusions.sulis.domain
+
+class Player {
+
+	String name
+	
+    static constraints = {
+		name nullable: false, size: 2..30
+    }
+}
---
+++ webapp/sulis/test/unit/moonillusions/sulis/domain/unit/PlayerTests.groovy
@@ -0,0 +1,60 @@
+
+package moonillusions.sulis.domain.unit
+
+import grails.test.mixin.*
+import liquibase.exception.SetupException;
+import moonillusions.grails.testing.matchers.FieldErrors;
+import moonillusions.sulis.domain.Player;
+
+import org.junit.*
+
+import static moonillusions.grails.testing.matchers.FieldErrors.fieldErrors;
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Player)
+@Mock(Player)
+class PlayerTests {
+
+	Player player
+	
+	@Before
+	void setup() {
+		player = new Player(name: "Kalle")
+	}
+	
+	@Test
+	void happyCase() {
+		assert(player.save())
+		assertThat(player, fieldErrors([:]))
+	}
+	
+	@Test
+	void nullConstraints() {
+		player.name = null
+		assert(!player.save())
+		assertThat(player, fieldErrors(name: "nullable"))
+	}
+	
+	@Test
+	void sizeMinConstraints() {
+		player.name = "a"
+		assert(!player.save())
+		assertThat(player, fieldErrors(name: "size.toosmall"))
+		player.name = "ab"
+		assert(player.save())
+		assertThat(player, fieldErrors([:]))
+	}
+	
+	@Test
+	void sizeMaxConstraints() {
+		player.name = "1234567890123456789012345678901"
+		assert(!player.save())
+		assertThat(player, fieldErrors(name: "size.toobig"))
+		player.name = "123456789012345678901234567890"
+		assert(player.save())
+		assertThat(player, fieldErrors([:]))
+	}
+   
+}
