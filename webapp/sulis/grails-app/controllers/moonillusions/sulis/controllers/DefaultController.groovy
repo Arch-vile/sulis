31a0f21a08b8abf26bc0324897f73eb9ebc85884
ra1p3 <raipe.spamfilter@gmail.com>
2013-12-06 14:41:09 +0200

    NEW: Players as model

--- webapp/sulis/grails-app/controllers/moonillusions/sulis/controllers/DefaultController.groovy
+++ webapp/sulis/grails-app/controllers/moonillusions/sulis/controllers/DefaultController.groovy
@@ -1,10 +1,13 @@
 package moonillusions.sulis.controllers
 
 import moonillusions.sulis.domain.Player
+import moonillusions.sulis.service.PlayerService;
 
 class DefaultController {
+	
+	PlayerService playerService
 
     def index() { 
-		render(view: 'home', model: [players: []])
+		render(view: 'home', model: [players: playerService.list()])
 	}
 }
--- webapp/sulis/test/unit/moonillusions/sulis/controllers/DefaultControllerSpec.groovy
+++ webapp/sulis/test/unit/moonillusions/sulis/controllers/DefaultControllerSpec.groovy
@@ -1,11 +1,18 @@
+
 package moonillusions.sulis.controllers
 
 import moonillusions.sulis.domain.Player;
+import moonillusions.sulis.service.PlayerService;
+import static org.hamcrest.Matchers.containsInAnyOrder
 
 import org.spockframework.compiler.model.ThenBlock;
 
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+
+import static moonillusions.grails.testing.matchers.ControllerView.renders
+import static moonillusions.grails.testing.matchers.ControllerModel.hasModel
+import static spock.util.matcher.HamcrestSupport.that
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
@@ -13,7 +20,10 @@ import spock.lang.Specification
 @TestFor(DefaultController)
 class DefaultControllerSpec extends Specification {
 
+	PlayerService playerService = Mock(PlayerService)
+	
 	def setup() {
+		controller.playerService = playerService
 	}
 
 	def cleanup() {
@@ -24,19 +34,20 @@ class DefaultControllerSpec extends Specification {
 		controller.index()
 		
 		then:
-		controller.modelAndView.viewName == "/default/home"
+		that controller, renders("/default/home")
 	}
 	
-	void "Index action should have players a model"() {
+	void "Index action should have all players in model"() {
 		setup:
 		Player player1 = new Player()
 		Player player2 = new Player()
+		playerService.list() >> [player1,player2]
 		
 		when:
 		controller.index()
 		
 		then:
-		controller.modelAndView.modelMap['players'] == []
+		that controller, hasModel('players',containsInAnyOrder(player2, player1))
 	}
 	
 }
