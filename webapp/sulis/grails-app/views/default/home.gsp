3d2c6c1e5159fb3d10bb1b4535641efac1ef7eb6
ra1p3 <raipe.spamfilter@gmail.com>
2014-07-19 15:19:26 +0300

    Field for adding the starting player

--- webapp/sulis/grails-app/views/default/home.gsp
+++ webapp/sulis/grails-app/views/default/home.gsp
@@ -3,3 +3,4 @@
 </p>
 
 <g:select name="startingPlayer" from="${players}" optionKey="name" optionValue="name" />
+or new  one <g:textField name="newStartingPlayer"/>
--- webapp/sulis/test/unit/moonillusions/sulis/views/DefaultIndexSpec.groovy
+++ webapp/sulis/test/unit/moonillusions/sulis/views/DefaultIndexSpec.groovy
@@ -2,30 +2,25 @@
 
 package moonillusions.sulis.views
 
-import moonillusions.sulis.controllers.DefaultController;
-import moonillusions.sulis.domain.Player;
-import moonillusions.sulis.service.PlayerService;
-import static org.hamcrest.Matchers.containsInAnyOrder
-
-import org.spockframework.compiler.model.ThenBlock;
-
-import grails.test.mixin.TestFor
-import grails.test.mixin.TestMixin;
-import grails.test.mixin.web.GroovyPageUnitTestMixin;
-import spock.lang.Specification
-import static moonillusions.grails.testing.matchers.ControllerView.renders
-import static moonillusions.grails.testing.matchers.ControllerModel.hasModel
-import static org.hamcrest.Matchers.equalTo
-import static spock.util.matcher.HamcrestSupport.that
 import static com.moonillusions.htmlUnitMatchers.matchers.HasOption.hasOption
+import static moonillusions.grails.testing.matchers.ControllerModel.hasModel
+import static moonillusions.grails.testing.matchers.ControllerView.renders
+import static org.hamcrest.Matchers.containsInAnyOrder
+import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.core.IsNull.nullValue
+import static org.hamcrest.Matchers.not
+import static spock.util.matcher.HamcrestSupport.that
+import grails.test.mixin.TestMixin
+import grails.test.mixin.web.GroovyPageUnitTestMixin
+import moonillusions.sulis.domain.Player
+import spock.lang.Specification
 
 import com.gargoylesoftware.htmlunit.StringWebResponse
 import com.gargoylesoftware.htmlunit.WebClient
-import com.gargoylesoftware.htmlunit.html.HtmlPage
 import com.gargoylesoftware.htmlunit.html.HTMLParser
-import com.gargoylesoftware.htmlunit.html.HtmlElement
-import com.gargoylesoftware.htmlunit.html.HtmlSelect;
-import com.gargoylesoftware.htmlunit.html.HtmlSpan
+import com.gargoylesoftware.htmlunit.html.HtmlInput
+import com.gargoylesoftware.htmlunit.html.HtmlPage
+import com.gargoylesoftware.htmlunit.html.HtmlSelect
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
@@ -34,7 +29,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlSpan
 class DefaultIndexSpec extends Specification {
 
 	String output
-	
+
 	def setup() {
 		output = ""
 	}
@@ -43,45 +38,57 @@ class DefaultIndexSpec extends Specification {
 	}
 
 	void "Home page should say hello"() {
-		
+
 		setup:
 		output = renderViewWithModel()
-		
+
 		when:
 		String text = asText('//span')[0]
-		
+
 		then:
 		that text, equalTo("hello!")
-		
 	}
-	
+
 	void "startingPlayer selection lists players"() {
-		
+
 		setup:
-		output = renderViewWithModel([players: [new Player(name: "mikko"), new Player(name: "sagi")]])
-		
+		output = renderViewWithModel([players: [
+				new Player(name: "mikko"),
+				new Player(name: "sagi")
+			]])
+
 		when:
-		HtmlSelect player1Selection = getElement("//select[@id='startingPlayer']")
-		
+		HtmlSelect player1Selection = getElement("//select[@name='startingPlayer']")
+
 		then:
 		that player1Selection, hasOption("mikko", "mikko", 0);
 		that player1Selection, hasOption("sagi", "sagi", 1);
 	}
-	
-	
+
+	void "input field for new starting player"() {
+		setup:
+		output = renderViewWithModel()
+		
+		when:
+		HtmlInput input = getElement("//input[@name='newStartingPlayer']")
+
+		then:
+		that input, not(nullValue())
+	}
+
+
 	private getElement(xpath) {
 		StringWebResponse resp = new StringWebResponse(output, new URL("http://localhost"))
 		WebClient client = new WebClient()
 		HtmlPage page = HTMLParser.parseHtml(resp, client.getCurrentWindow())
 		page.getFirstByXPath(xpath)
 	}
-	
+
 	private asText(xpath) {
 		getElement(xpath).collect { it.asText() }
 	}
-	
+
 	private renderViewWithModel(myModel = [:]) {
 		render(view: '/default/home', model: myModel)
 	}
-	
 }
