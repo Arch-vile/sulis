8c7366c6523edf68ecfb33f0f62abd8b9ca657d6
ra1p3 <raipe.spamfilter@gmail.com>
2014-07-19 14:08:36 +0300

    Upgrade to Grails 2.4.2

--- setGrails.sh
+++ setGrails.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export GRAILS_HOME=/home/rapsu/Downloads/grails-2.3.4
+export GRAILS_HOME=/home/rapsu/programs/ggts-bundle/grails-2.4.2
 export PATH=$PATH:$GRAILS_HOME/bin
