21d10b1e76fd5ac7b4c403575124984da6017abf
ra1p3 <raipe.spamfilter@gmail.com>
2014-07-27 20:49:06 +0300

    Base class for doing view spec tests

---
+++ webapp/sulis/src/groovy/moonillusions/sulis/testing/HtmlUnitViewSpec.groovy
@@ -0,0 +1,46 @@
+package moonillusions.sulis.testing
+
+import com.gargoylesoftware.htmlunit.StringWebResponse;
+import com.gargoylesoftware.htmlunit.html.HTMLParser;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.WebClient
+
+import spock.lang.Specification;
+
+class HtmlUnitViewSpec extends Specification {
+
+	def getElement(xpath, html) {
+		assert xpath, "xpath must not be empty"
+		assert html, "html must not be empty"
+		
+		StringWebResponse resp = new StringWebResponse(html, new URL("http://localhost"))
+		WebClient client = new WebClient()
+		HtmlPage page = HTMLParser.parseHtml(resp, client.getCurrentWindow())
+		page.getFirstByXPath(xpath)
+	}
+
+	
+	def renderViewWithModel(arguments = [:]) {
+		def view = arguments.view ?: defaultName()
+		assert view, "No view defined. Introduce 'view' class variable or give as method argument"
+		
+		def output = render(view: view, model: arguments.model)
+		assert output, "Rendering view '${view}' produced no output"
+		
+		if(arguments.xpath) {
+			return getElement(arguments.xpath, output)
+		}
+		return output
+	}
+	
+	
+	def defaultName() {
+		def className = this.class.getCanonicalName()
+		if(className.endsWith("Spec")) {
+			def namePart = className.substring(0, className.lastIndexOf("Spec"))
+			return '/' + namePart.findAll( /([A-Z]+[a-z]*)/ ).join('/').toLowerCase()
+		}
+		
+	}
+	
+}
