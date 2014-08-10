4a49505f9667fb78d244dc620afb85129bfd49d0
ra1p3 <raipe.spamfilter@gmail.com>
2014-08-10 14:00:21 +0300

    Add tests for class from 3rd party class and failure scenarious

--- webapp/sulis/src/java/SafeNavigationTest.java
+++ webapp/sulis/src/java/SafeNavigationTest.java
@@ -10,48 +10,58 @@ public class SafeNavigationTest {
 	
 	@Test
 	public void public_variable() {
-		assertThat(prop(MyTestClass.class, "name"), equalTo("name"));
+		assertThat(prop(Book.class, "title"), equalTo("title"));
 	}
 	
 	@Test
 	public void public_getter() {
-		assertThat(prop(MyTestClass.class, "age"), equalTo("age"));
+		assertThat(prop(Book.class, "pageCount"), equalTo("pageCount"));
 	}
 	
 	@Test
 	public void deep_variable() {
-		assertThat(prop(MyTestClass.class, "deep.worldPeace"), equalTo("deep.worldPeace"));
+		assertThat(prop(Book.class, "author.age"), equalTo("author.age"));
 	}
 	
 	@Test
 	public void deep_getter() {
-		assertThat(prop(MyTestClass.class, "forGetter.type"), equalTo("forGetter.type"));
+		assertThat(prop(Book.class, "author.name"), equalTo("author.name"));
+	}
+	
+	@Test
+	public void class_from_another_package() {
+		assertThat(prop(System.class, "out"), equalTo("out"));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void fail_simple() {
+		prop(Book.class, "titleNot");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void fail_deep() {
+		prop(Book.class, "author.noSuchProp");
 	}
 	
 	
-	private class MyTestClass {
+	private class Book {
 		
-		public String name;
-		private Integer age;
-		public MyNestedClass deep;
-		private MyNestedClass forGetter;
+		public String title;
+		private Integer pageCount;
+		public Author author;
 		
-		public Integer getAge() {
-			return this.age;
-		}
-		
-		public MyNestedClass getForGetter() {
-			return this.forGetter;
+		public Integer getPageCount() {
+			return this.pageCount;
 		}
 		
 	}
 	
-	private class MyNestedClass {
-		public Boolean worldPeace;
-		private String type;
+	private class Author {
+		private String name;
+		public Integer age;
 		
-		public String getType() {
-			return this.type;
+		public String getName() {
+			return this.name;
 		}
 	}
 
